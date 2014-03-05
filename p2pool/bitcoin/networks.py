f41071bfe728bbe507bdaf11eaaa6b7691a634a5
P2PHash <p2phash@gmail.com>
2014-03-05 17:43:15 +0100

    initial commit

---
+++ p2pool/bitcoin/networks.py
@@ -0,0 +1,45 @@
+import os
+import platform
+
+from twisted.internet import defer
+
+from . import data
+from p2pool.util import math, pack, jsonrpc
+
+@defer.inlineCallbacks
+def check_genesis_block(bitcoind, genesis_block_hash):
+    try:
+        yield bitcoind.rpc_getblock(genesis_block_hash)
+    except jsonrpc.Error_for_code(-5):
+        defer.returnValue(False)
+    else:
+        defer.returnValue(True)
+
+nets = dict(
+    Credits=math.Object(
+        P2P_PREFIX='deb5a3ba'.decode('hex'), #pchmessagestart
+        P2P_PORT=51119,
+        ADDRESS_VERSION=28, #pubkey_address
+        RPC_PORT=51118,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'Creditsaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 796*20000000000 >> (height + 1)//1577856,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # seconds
+        SYMBOL='CR',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Credits')
+                if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Credits/')
+                if platform.system() == 'Darwin' else os.path.expanduser('~/.Credits'), 'Credits.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer1.cloudapp.net/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer1.cloudapp.net/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer1.cloudapp.net/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
+
+)
+for net_name, net in nets.iteritems():
+    net.NAME = net_name
---
+++ p2pool/networks.py
@@ -0,0 +1,32 @@
+from p2pool.bitcoin import networks
+from p2pool.util import math
+
+# CHAIN_LENGTH = number of shares back client keeps
+# REAL_CHAIN_LENGTH = maximum number of shares back client uses to compute payout
+# REAL_CHAIN_LENGTH must always be <= CHAIN_LENGTH
+# REAL_CHAIN_LENGTH must be changed in sync with all other clients
+# changes can be done by changing one, then the other
+
+nets = dict(
+    Credits=math.Object(
+        PARENT=networks.nets['Credits'],
+        SHARE_PERIOD=10, # seconds
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares
+        SPREAD=45, # blocks
+        IDENTIFIER='1a51e44306bb9bcf'.decode('hex'),
+        PREFIX='6cef97eb4a9a31cf'.decode('hex'),
+        P2P_PORT=55017,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=True,
+        WORKER_PORT=55018,
+        BOOTSTRAP_ADDRS='p2phash.com'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+    ),
+
+)
+for net_name, net in nets.iteritems():
+    net.NAME = net_name
